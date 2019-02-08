a63790c4443cd43cd5c01bd95ff9530dcaccd303
sextocomputacion <44627436+MarceloRosalesCastillo@users.noreply.github.com>
2019-02-08 16:12:30 -0500

    Update group.js

--- models/group.js
+++ models/group.js
@@ -5,7 +5,7 @@ module.exports = (sequelize, DataTypes) => {
   }, {});
   Group.associate = function(models) {
     // associations can be defined here
-    Group.hasMany(models.UserGroup);
+    models.Group.hasMany(models.UserGroup);
   };
   return Group;
 };
