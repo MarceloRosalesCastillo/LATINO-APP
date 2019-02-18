9265dbd85e023c55fb2d9ef1f4608e6253771e64
Marcelo <i1610110@continental.edu.pe>
2019-02-18 12:22:44 -0500

    conne

--- src/keys.js
+++ src/keys.js
@@ -2,10 +2,12 @@ module.exports = {
 
     database:{
 
-        host: 'localhost',
-        user: 'root',
-        password: null,
-        database: 'db_latino'
+        host: 'https://web520.webfaction.com/phpMyAdmin',
+        port: 3306,
+        user: 'latino_host56725',
+        password: 'host56725@latino1',
+        database: 'latino_host56725a',
+        
 
     }
 
