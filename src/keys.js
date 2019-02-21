2df6b7d6e59eee2053ce214d4d72361362ee76c7
Marcelo <i1610110@continental.edu.pe>
2019-02-21 15:00:56 -0500

    olvide el host

--- src/keys.js
+++ src/keys.js
@@ -1,29 +1,29 @@
-module.exports = {
-
-    database:{
-
-        host: 'localhost',
-        user: 'root',
-        password: null,
-        database: 'db_latino',
-        
-
-    }
-
-
-}
 // module.exports = {
 
 //     database:{
 
-//         host: 'https://web520.webfaction.com/phpMyAdmin',
-//         port: 3306,
-//         user: 'latino_host56725',
-//         password: 'host56725@latino1',
-//         database: 'latino_host56725a',
+//         host: 'localhost',
+//         user: 'root',
+//         password: null,
+//         database: 'db_latino',
         
 
 //     }
+
+
+// }
+module.exports = {
+
+    database:{
+
+        host: 'https://web520.webfaction.com/phpMyAdmin',
+        port: 3306,
+        user: 'latino_host56725',
+        password: 'host56725@latino1',
+        database: 'latino_host56725a',
+        
+
+    }
 
 
 // }
